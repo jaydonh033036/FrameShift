8dc2baee5c2cd7d9aaa7710b170ea54d487a8c44
jaydonh033036 <jaydon.hodson@gmail.com>
2026-08-27 00:27:24 +0100

    added back the image average using a bin and lib insted constructing a cli to accompany the procedure

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,18 @@
+use clap::Parser;
+use gutter_detection::average;
+
+#[derive(Parser)]
+#[command(name = "cli", about = "Gutter detection CLI")]
+struct Args {
+    /// Input image file(s) to process
+    #[arg(short = 'i', long = "input", num_args = 1..)]
+    input: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    for file in &args.input {
+        average(file);
+    }
 }
--- gutter_detection/src/lib.rs
+++ gutter_detection/src/lib.rs
@@ -1,14 +1,35 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use image::{ImageReader,RgbImage, Rgb};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn average(file_name: &str) {
+    let img = ImageReader::open(file_name)
+        .unwrap()
+        .decode()
+        .unwrap()
+        .to_rgb8();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    let width = img.width() as u64;
+    let height = img.height() as u64;
+
+    println!("Width: {}", width);
+    println!("Height: {} \n", height);
+
+    let  num_pixels:u64 = width * height;
+    let mut tot_rgb:[u64; 3] = [0, 0, 0];
+
+    for pixel in img.pixels() {
+        let [r, g, b] = pixel.0;
+
+        tot_rgb[0] += r as u64;
+        tot_rgb[1] += g as u64;
+        tot_rgb[2] += b as u64;
     }
+
+    let avg_colour:[u8;3] = [
+        (tot_rgb[0] / num_pixels) as u8,
+        (tot_rgb[1] / num_pixels) as u8,
+        (tot_rgb[2] / num_pixels) as u8,
+    ];
+
+    println!("Total Pixels: {}", num_pixels);
+    println!("Average Colour: {:?}", avg_colour);
 }
